c19e6c9d73db2587a95be82e8016d4e907fadf40
shm <m11c3.sh@gmail.com>
2025-01-13 18:15:26 +0900

    Refactor: Fix empty line after outer attribute

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#[macro_use]
+#![macro_use]
 
 mod commands;
 mod enums;
