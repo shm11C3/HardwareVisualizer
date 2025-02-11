9d72fc80065514d7ea43b9cd7584dac8bcc579ed
shm <m11c3.sh@gmail.com>
2025-02-11 12:30:18 +0900

    カバレッジ出力設定の追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,18 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
-    include: ["src/test/**/*.test.ts"],
+    include: ["src/test/unit/**/*.test.ts"],
+    coverage: {
+      include: ["src/**/*.ts"],
+      exclude: ["src/rspc/**"],
+      reporter: ["text", "html", "json-summary"],
+      thresholds: {
+        statements: 60,
+        branches: 60,
+        functions: 60,
+        lines: 60,
+      },
+    },
   },
   resolve: {
     alias: {
