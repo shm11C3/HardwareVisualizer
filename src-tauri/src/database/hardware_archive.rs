6d55c2ade1f5ffa76657644dd0727ab308f72189
shm <m11c3.sh@gmail.com>
2025-02-16 21:01:55 +0900

    Fix: query

--- src-tauri/src/database/hardware_archive.rs
+++ src-tauri/src/database/hardware_archive.rs
@@ -7,14 +7,14 @@ pub async fn insert(
   ram: structs::hardware_archive::HardwareData,
 ) -> Result<(), sqlx::Error> {
   let dir_path = utils::file::get_app_data_dir("hv-database.db");
-  let database_url = format!("sqlite::{}", dir_path.to_string_lossy());
+  let database_url = format!("sqlite:{}", dir_path.to_str().unwrap());
 
   let pool = SqlitePool::connect(&database_url).await?;
 
   sqlx::query(
 
     "INSERT INTO DATA_ARCHIVE (cpu_avg, cpu_max, cpu_min, ram_avg, ram_max, ram_min, timestamp)
-    VALUES ($1, $2, $3, $4)",
+    VALUES ($1, $2, $3, $4, $5, $6, $7)",
   ).bind(0).bind(cpu.avg).bind(cpu.max).bind(cpu.min).bind(ram.avg).bind(ram.max).bind(ram.min).bind(chrono::Utc::now().timestamp()).execute(&pool).await?;
 
   Ok(())
