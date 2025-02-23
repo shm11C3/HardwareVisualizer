fe5a0e3b7f66da354978a07105b839be53d38687
shm <m11c3.sh@gmail.com>
2025-02-23 19:07:06 +0900

    Refactor: SingleLineChart

--- src/components/charts/insights/InightChart.tsx
+++ src/components/charts/insights/InightChart.tsx
@@ -1,5 +1,8 @@
-import { LineChartComponent } from "@/components/charts/LineChart";
+import { useSettingsAtom } from "@/atom/useSettingsAtom";
+import { SingleLineChart } from "@/components/charts/LineChart";
+import type { ChartConfig } from "@/components/ui/chart";
 import type { ShowDataType } from "@/types/chart";
+import type { ChartDataType } from "@/types/hardwareDataType";
 import { useInsightChart } from "./useInsightChart";
 
 const ShowDataType2ChartType: Record<ShowDataType, "cpu" | "memory" | "gpu"> = {
@@ -14,20 +17,43 @@ const ShowDataType2ChartType: Record<ShowDataType, "cpu" | "memory" | "gpu"> = {
 export const InsightChart = ({
   dataType,
 }: { dataType: "cpu" | "memory" | "gpu" }) => {
+  const { settings } = useSettingsAtom();
+
   const { labels, chartData } = useInsightChart({
     type: "cpu_avg",
     endAt: new Date(),
     period: 180,
   });
 
+  const chartConfig: Record<ChartDataType, { label: string; color: string }> = {
+    cpu: {
+      label: "CPU",
+      color: `rgb(${settings.lineGraphColor.cpu})`,
+    },
+    memory: {
+      label: "RAM",
+      color: `rgb(${settings.lineGraphColor.memory})`,
+    },
+    gpu: {
+      label: "GPU",
+      color: `rgb(${settings.lineGraphColor.gpu})`,
+    },
+  } satisfies ChartConfig;
+
   return (
     <div>
-      <LineChartComponent
+      <SingleLineChart
         labels={labels}
         chartData={chartData}
         dataType={dataType}
+        chartConfig={chartConfig}
+        border={false}
         size="xl"
         lineGraphMix={false}
+        lineGraphShowScale={true}
+        lineGraphShowTooltip={true}
+        lineGraphType={settings.lineGraphType}
+        lineGraphShowLegend={false}
       />
     </div>
   );
