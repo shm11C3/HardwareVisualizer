69a2230e907152c3feb001d20e749f67e1fca6b1
shm <m11c3.sh@gmail.com>
2025-03-09 05:18:30 +0900

    リファクタ

--- src-tauri/src/services/hardware_archive_service.rs
+++ src-tauri/src/services/hardware_archive_service.rs
@@ -18,64 +18,9 @@ pub async fn start_hardware_archive_service(
       // 1分待機
       thread::sleep(Duration::from_secs(HISTORY_CAPACITY as u64));
 
-      let cpu_avg = {
-        let cpu_hist = cpu_history.lock().unwrap();
-        if cpu_hist.is_empty() {
-          0.0
-        } else {
-          cpu_hist.iter().sum::<f32>() / cpu_hist.len() as f32
-        }
-      };
-
-      let cpu_max = cpu_history
-        .lock()
-        .unwrap()
-        .iter()
-        .cloned()
-        .fold(0.0, f32::max);
-
-      let cpu_min = cpu_history
-        .lock()
-        .unwrap()
-        .iter()
-        .cloned()
-        .fold(100.0, f32::min);
-
-      let memory_avg = {
-        let mem_hist = memory_history.lock().unwrap();
-        if mem_hist.is_empty() {
-          0.0
-        } else {
-          mem_hist.iter().sum::<f32>() / mem_hist.len() as f32
-        }
-      };
-
-      let memory_max = memory_history
-        .lock()
-        .unwrap()
-        .iter()
-        .cloned()
-        .fold(0.0, f32::max);
-
-      let memory_min = memory_history
-        .lock()
-        .unwrap()
-        .iter()
-        .cloned()
-        .fold(100.0, f32::min);
-
       // 現在時刻と平均値を用いてアーカイブデータを生成
-      let cpu_data = structs::hardware_archive::HardwareData {
-        avg: cpu_avg,
-        max: cpu_max,
-        min: cpu_min,
-      };
-
-      let memory_data = structs::hardware_archive::HardwareData {
-        avg: memory_avg,
-        max: memory_max,
-        min: memory_min,
-      };
+      let cpu_data = get_cpu_data(cpu_history.clone());
+      let memory_data = get_memory_data(memory_history.clone());
 
       let result = hardware_archive::insert(cpu_data, memory_data).await;
       if let Err(e) = result {
@@ -105,4 +50,70 @@ pub async fn batch_delete_old_data(refresh_interval_days: u32) {
       );
     }
   });
+}
+
+fn get_cpu_data(
+  cpu_history: Arc<Mutex<VecDeque<f32>>>,
+) -> structs::hardware_archive::HardwareData {
+  let cpu_avg = {
+    let cpu_hist = cpu_history.lock().unwrap();
+    if cpu_hist.is_empty() {
+      0.0
+    } else {
+      cpu_hist.iter().sum::<f32>() / cpu_hist.len() as f32
+    }
+  };
+
+  let cpu_max = cpu_history
+    .lock()
+    .unwrap()
+    .iter()
+    .cloned()
+    .fold(0.0, f32::max);
+
+  let cpu_min = cpu_history
+    .lock()
+    .unwrap()
+    .iter()
+    .cloned()
+    .fold(100.0, f32::min);
+
+  structs::hardware_archive::HardwareData {
+    avg: cpu_avg,
+    max: cpu_max,
+    min: cpu_min,
+  }
+}
+
+fn get_memory_data(
+  memory_history: Arc<Mutex<VecDeque<f32>>>,
+) -> structs::hardware_archive::HardwareData {
+  let memory_avg = {
+    let mem_hist = memory_history.lock().unwrap();
+    if mem_hist.is_empty() {
+      0.0
+    } else {
+      mem_hist.iter().sum::<f32>() / mem_hist.len() as f32
+    }
+  };
+
+  let memory_max = memory_history
+    .lock()
+    .unwrap()
+    .iter()
+    .cloned()
+    .fold(0.0, f32::max);
+
+  let memory_min = memory_history
+    .lock()
+    .unwrap()
+    .iter()
+    .cloned()
+    .fold(100.0, f32::min);
+
+  structs::hardware_archive::HardwareData {
+    avg: memory_avg,
+    max: memory_max,
+    min: memory_min,
+  }
 }
