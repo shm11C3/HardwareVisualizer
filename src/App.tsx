289dc641c77ce94f89d898898cdaae49b0177862
shm <m11c3.sh@gmail.com>
2025-03-26 00:37:00 +0900

    Fix: Build Error

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react";
-import Dashboard from "./template/Dashboard";
-import ChartTemplate from "./template/Usage";
+import Dashboard from "./features/dashboard/Dashboard";
+import ChartTemplate from "./features/usage/Usage";
 import "./index.css";
 import { useHardwareUpdater, useUsageUpdater } from "@/hooks/useHardwareData";
 import { useErrorModalListener } from "@/hooks/useTauriEventListener";
 import type { ErrorInfo, JSX } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./components/ErrorFallback";
+import ScreenTemplate from "./components/shared/ScreenTemplate";
+import { SideMenu, displayTargetAtom } from "./features/menu/SideMenu";
+import Settings from "./features/settings/Settings";
 import { useBackgroundImage } from "./hooks/useBgImage";
 import { useDarkMode } from "./hooks/useDarkMode";
 import { useSettingsAtom } from "./hooks/useSettingsAtom";
-import ScreenTemplate from "./template/ScreenTemplate";
-import Settings from "./template/Settings";
-import { SideMenu, displayTargetAtom } from "./template/SideMenu";
 import type { SelectedDisplayType } from "./types/ui";
 import "@/lib/i18n";
 import { useAtom } from "jotai";
 import { useTranslation } from "react-i18next";
+import { Insights } from "./features/insights/Insights";
 import { useKeydown } from "./hooks/useInputListener";
-import { Insights } from "./template/Insights";
 
 const onError = (error: Error, info: ErrorInfo) => {
   console.error("error.message", error.message);
