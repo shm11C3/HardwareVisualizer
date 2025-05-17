69fe92dd54171f4c45f3800993f789d78eff31a8
shm <m11c3.sh@gmail.com>
2025-05-18 00:54:29 +0900

    Fix: adjust padding and margin in ScreenTemplate component for better layout

--- src/components/shared/ScreenTemplate.tsx
+++ src/components/shared/ScreenTemplate.tsx
@@ -5,7 +5,7 @@ interface ScreenTemplateProps {
 
 const ScreenTemplate: React.FC<ScreenTemplateProps> = ({ title, children }) => {
   return (
-    <div className="mx-auto w-full px-4 pt-12 2xl:w-3/4">
+    <div className="mx-auto w-full pt-12 pr-4 pl-20 2xl:w-3/4 2xl:px-4">
       {title && <h2 className="py-3 font-bold text-3xl"> {title}</h2>}
       {children}
     </div>
