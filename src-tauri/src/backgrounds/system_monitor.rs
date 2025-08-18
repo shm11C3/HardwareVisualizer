77c3392ffa93af976d9a3391deb2496a985a4f1a
shm <m11c3.sh@gmail.com>
2025-08-19 02:15:03 +0900

    refactor(system_monitor): streamline resource handling in setup function

--- src-tauri/src/backgrounds/system_monitor.rs
+++ src-tauri/src/backgrounds/system_monitor.rs
@@ -15,30 +15,6 @@ const SYSTEM_INFO_INIT_INTERVAL: u64 = 1;
 /// - `SYSTEM_INFO_INIT_INTERVAL` 秒ごとにCPU使用率とメモリ使用率を更新
 ///
 pub async fn setup(resources: structs::hardware_archive::MonitorResources) {
-  #[cfg(target_os = "windows")]
-  let structs::hardware_archive::MonitorResources {
-    system,
-    cpu_history,
-    memory_history,
-    process_cpu_histories,
-    process_memory_histories,
-    nv_gpu_usage_histories,
-    nv_gpu_temperature_histories,
-    nv_gpu_dedicated_memory_histories,
-  } = resources;
-
-  #[cfg(target_os = "linux")]
-  let structs::hardware_archive::MonitorResources {
-    system,
-    cpu_history,
-    memory_history,
-    process_cpu_histories,
-    process_memory_histories,
-    nv_gpu_usage_histories: _nv_gpu_usage_histories,
-    nv_gpu_temperature_histories: _nv_gpu_temperature_histories,
-    nv_gpu_dedicated_memory_histories: _nv_gpu_dedicated_memory_histories,
-  } = resources;
-
   let mut interval =
     tokio::time::interval(Duration::from_secs(SYSTEM_INFO_INIT_INTERVAL));
 
@@ -46,53 +22,27 @@ pub async fn setup(resources: structs::hardware_archive::MonitorResources) {
     interval.tick().await;
 
     {
-      // サンプリングをサービスへ委譲 (内部で system をロック)
+      let monitor_resources = structs::hardware_archive::MonitorResources {
+        system: resources.system.clone(),
+        cpu_history: resources.cpu_history.clone(),
+        memory_history: resources.memory_history.clone(),
+        process_cpu_histories: resources.process_cpu_histories.clone(),
+        process_memory_histories: resources.process_memory_histories.clone(),
+        nv_gpu_usage_histories: resources.nv_gpu_usage_histories.clone(),
+        nv_gpu_temperature_histories: resources.nv_gpu_temperature_histories.clone(),
+        nv_gpu_dedicated_memory_histories: resources
+          .nv_gpu_dedicated_memory_histories
+          .clone(),
+      };
+
       #[cfg(target_os = "windows")]
       {
-        monitoring_service::sample_system(&structs::hardware_archive::MonitorResources {
-          system: system.clone(),
-          cpu_history: cpu_history.clone(),
-          memory_history: memory_history.clone(),
-          process_cpu_histories: process_cpu_histories.clone(),
-          process_memory_histories: process_memory_histories.clone(),
-          nv_gpu_usage_histories: nv_gpu_usage_histories.clone(),
-          nv_gpu_temperature_histories: nv_gpu_temperature_histories.clone(),
-          nv_gpu_dedicated_memory_histories: nv_gpu_dedicated_memory_histories.clone(),
-        });
-        monitoring_service::sample_gpu(&structs::hardware_archive::MonitorResources {
-          system: system.clone(),
-          cpu_history: cpu_history.clone(),
-          memory_history: memory_history.clone(),
-          process_cpu_histories: process_cpu_histories.clone(),
-          process_memory_histories: process_memory_histories.clone(),
-          nv_gpu_usage_histories: nv_gpu_usage_histories.clone(),
-          nv_gpu_temperature_histories: nv_gpu_temperature_histories.clone(),
-          nv_gpu_dedicated_memory_histories: nv_gpu_dedicated_memory_histories.clone(),
-        });
+        monitoring_service::sample_system(&monitor_resources);
+        monitoring_service::sample_gpu(&monitor_resources);
       }
       #[cfg(target_os = "linux")]
       {
-        // Linux では GPU 履歴を未使用のため空のマップを再利用
-        use std::collections::{HashMap, VecDeque};
-        use std::sync::{Arc, Mutex};
-        monitoring_service::sample_system(&structs::hardware_archive::MonitorResources {
-          system: system.clone(),
-          cpu_history: cpu_history.clone(),
-          memory_history: memory_history.clone(),
-          process_cpu_histories: process_cpu_histories.clone(),
-          process_memory_histories: process_memory_histories.clone(),
-          nv_gpu_usage_histories: Arc::new(Mutex::new(
-            HashMap::<String, VecDeque<f32>>::new(),
-          )),
-          nv_gpu_temperature_histories: Arc::new(Mutex::new(HashMap::<
-            String,
-            VecDeque<i32>,
-          >::new())),
-          nv_gpu_dedicated_memory_histories: Arc::new(Mutex::new(HashMap::<
-            String,
-            VecDeque<i32>,
-          >::new())),
-        });
+        monitoring_service::sample_system(&monitor_resources);
       }
     }
   }
