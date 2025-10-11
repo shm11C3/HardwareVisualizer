ae61b0b0e7edd450f5751a64d839a785970553e7
shm <m11c3.sh@gmail.com>
2025-10-12 01:09:30 +0900

    chore: update recharts to version 3.2.1 and enhance tooltip types in chart components (#739)

--- src/features/hardware/insights/process/chart/components/CustomTooltip.tsx
+++ src/features/hardware/insights/process/chart/components/CustomTooltip.tsx
@@ -1,18 +1,23 @@
 import { CpuIcon, MemoryIcon, TimerIcon } from "@phosphor-icons/react";
 import { useTranslation } from "react-i18next";
-import type { TooltipProps } from "recharts";
-import type {
-  NameType,
-  ValueType,
-} from "recharts/types/component/DefaultTooltipContent";
 import { bubbleChartColor } from "@/features/hardware/consts/chart";
 import { useSettingsAtom } from "@/features/settings/hooks/useSettingsAtom";
 import { formatBytes } from "@/lib/formatter";
 
-export const CustomTooltip = ({
-  active,
-  payload,
-}: TooltipProps<ValueType, NameType>) => {
+type CustomTooltipProps = {
+  active?: boolean;
+  payload?: Array<{
+    payload: {
+      name: string;
+      pid: number;
+      x: number;
+      y: number;
+      ram: number;
+    };
+  }>;
+};
+
+export const CustomTooltip = ({ active, payload }: CustomTooltipProps) => {
   const { t } = useTranslation();
   const { settings } = useSettingsAtom();
 
