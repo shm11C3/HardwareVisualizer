bdd594ec97916f0bee9b54fc88a2c2890928e91a
shm11C3 <m11c3.sh@gmail.com>
2026-02-14 02:25:47 +0900

    Fix: hide motherboard component on unsupported OS

--- src/features/hardware/dashboard/components/DashboardItemSelector.tsx
+++ src/features/hardware/dashboard/components/DashboardItemSelector.tsx
@@ -1,4 +1,5 @@
 import { FunnelIcon } from "@phosphor-icons/react";
+import { platform } from "@tauri-apps/plugin-os";
 import { useTranslation } from "react-i18next";
 import {
   DropdownMenu,
@@ -21,6 +22,7 @@ export const DashboardItemSelector = ({
   toggleItem: (item: DashboardSelectItemType) => void;
 }) => {
   const { t } = useTranslation();
+  const os = platform();
 
   if (!visibleItems) return null;
 
@@ -35,7 +37,12 @@ export const DashboardItemSelector = ({
     motherboard: t("shared.motherboard"),
   };
 
-  const items: DashboardSelectItemType[] = ["title", ...dashBoardItems];
+  const items: DashboardSelectItemType[] = [
+    "title",
+    ...dashBoardItems.filter(
+      (item) => item !== "motherboard" || os === "windows",
+    ),
+  ];
 
   return (
     <DropdownMenu>
